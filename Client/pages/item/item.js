671d0cd86224ec0b0d01c66f7caee6358c508e1e
MikeBison <356981553@qq.com>
2017-01-03 00:06:15 +0800

    界面数据刷新
    界面数据刷新

--- Client/pages/item/item.js
+++ Client/pages/item/item.js
@@ -1,28 +1,55 @@
 //index.js
 //获取应用实例
+var data = {
+  title:"",
+  time:"",
+  date:"",
+  level:{
+    1:{value:1,checked:true,text:"一般"},
+    2:{value:2,checked:false,text:"重要"},
+    3:{value:3,checked:false,text:"紧急"}
+  }
+};
 var app = getApp()
 Page({
   data: {
-    item: {}
+    item: {
+      time:"",
+      date:""
+    },
+    time:data.time,
+    date:data.date,
+    level:data.level
   },
   //事件处理函数
-  bindViewTap: function() {
-    // wx.navigateTo({
-    //   url: '../logs/logs'
-    // })
-    console.log("likai")
+  titleInput: function(event) {
+    data["title"] = event.detail.value;
+    this.setData(data)
   },
-  levelChange: function (){
-
+  contentInput:function(event){
+    data["content"] = event.detail.value;
+    this.setData(data)
+  },
+  itemLevelChange: function (event){
+    for(var i in data.level){
+      data.level[i].checked=false;
+    }
+    data["level"][event.detail.value]["checked"] = true;
+    this.setData(data)
   },
   dateChange:function (){
 
   },
-  bindTimeChange: function (){
-
+  bindTimeChange: function (event){
+    data["time"] = event.detail.value;
+    this.setData(data)
   },
-  bindDateChange: function(){
-
+  bindDateChange: function(event){
+    data["date"] = event.detail.value;
+    this.setData(data)
+  },
+  submitItem:function(event){
+    //提交数据
   },
   onLoad: function () {
     console.log('onLoad')
